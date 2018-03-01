73a48bf19a8707fde2a9ef1c865a959cc45df1f0
kinderlisena <35637326+kinderlisena@users.noreply.github.com>
2018-03-01 16:54:58 -0300

    Add files via upload

---
+++ 202-dom-master/202-dom-master/scripts.js
@@ -0,0 +1,9 @@
+// alert('Qualquer coisa aqui');
+
+console.log('só um teste...');
+
+let quadrado = document.querySelector('div');
+
+function mudarQuadrado(){
+    quadrado.style.transform = 'rotateX(1000deg)';
+}
